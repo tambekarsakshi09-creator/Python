2d02462b86ef0e40dd92aa1eacf64b14c552ebf4
SakshiT <tambekarsakshi09@gmail.com>
2025-12-15 15:42:38 +0530

    Add files via upload

---
+++ Assignment 2 Task 1.py
@@ -0,0 +1,8 @@
+# Taking integer input from the user
+num = int(input("Enter an integer: "))
+
+# Checking if the number is even or odd
+if num % 2 == 0:
+    print(f"{num} is an Even number.")
+else:
+    print(f"{num} is an Odd number.")
